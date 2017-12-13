daf86fe0d9cf84be2e968690d40ce32f2434f0d3
peng.pang@zhiguoguo.com <123456789>
2017-12-13 17:08:02 +0800

    舞台基础搭建

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,15 +15,33 @@ imagesData = (function genImageURL(imageDatasArr){
   return imageDatasArr;
 })(imagesData);
 
+class ImageFigure extends React.Component {
+  render(){
+    return(
+      <figure className="img-figure">
+        <img src={this.props.data.imagesUrl} alt={this.props.data.title}/>
+        <figcaption>
+          <h2 className="img-title">{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    )
+  }
+}
+
 class AppComponent extends React.Component {
   render() {
+    let controllerUnits = [],
+      imgFigures = [];
+    imagesData.forEach(function (value,index) {
+      imgFigures.push(<ImageFigure data={value} key={'imgFigures'+index}/>);
+    });
     return (
       <section className="stage">
         <section className="img-sec">
-
+          {imgFigures}
         </section>
         <nav className="controller-nav">
-
+          {controllerUnits}
         </nav>
       </section>
     );
