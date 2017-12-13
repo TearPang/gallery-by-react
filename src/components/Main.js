99aaf85bf45051ade79a3656b7a93acb0a17ee60
peng.pang@zhiguoguo.com <123456789>
2017-12-13 18:00:43 +0800

    舞台基础搭建

--- src/components/Main.js
+++ src/components/Main.js
@@ -29,14 +29,61 @@ class ImageFigure extends React.Component {
 }
 
 class AppComponent extends React.Component {
+  //位置初始化
+  Constant:{
+    centerPos:{//中心点的位置
+      left: 0,
+      right: 0
+    },
+    hPosRange:{//水平方向的取值范围
+      leftSecX: [0,0],
+      rightSecX: [0,0],
+      y: [0,0]
+    },
+    vPosRange:{//垂直方向的取值范围
+      x: [0,0],
+      topY: [0,0]
+    }
+  };
+  //组件加载以后，为每张图片计算其位置d额范围
+  componentDidMount(){
+    //首先拿到舞台大小
+    let stageDOM = ReactDOM.findDOMNode(this.refs.stage),
+      stageW = stageDOM.scrollHeight,
+      stageH = stageDOM.scrollWidth,
+      halfStageW = Math.ceil(stageW/2),
+      halfStageH = Math.ceil(stageH/2);
+    //拿到imgFigure的大小
+    let imgFigureDOM = ReactDOM.findDOMNode(this.refs.imgFigure0),
+      imgW = imgFigureDOM.scrollWidth,
+      imgH = imgFigureDOM.scrollHeight,
+      halfImgW = Math.ceil(imgW/2),
+      halfImgH = Math.ceil(imgH/2);
+    //计算中心图片的位置点
+    this.Constant.centerPos = {
+      left: halfStageW - halfImgW,
+      right: halfStageH - halfImgH
+    },
+    //计算水平方向
+    this.Constant.hPosRange.leftSecX[0] =  -halfImgW;
+    this.Constant.hPosRange.leftSecX[1] = halfStageW-halfImgW*3;
+      rightSecX: [halfStageW+halfImgW,stageW-halfImgW],
+      y: [-halfImgH,stageH-halfImgH]
+    },
+    //垂直方向的取值范围
+    this.Constant.vPosRange = {
+       x: [0,0],
+       topY: [0,0]
+    }
+  }
   render() {
     let controllerUnits = [],
       imgFigures = [];
     imagesData.forEach(function (value,index) {
-      imgFigures.push(<ImageFigure data={value} key={'imgFigures'+index}/>);
+      imgFigures.push(<ImageFigure data={value} key={'imgFigures'+index} ref={'imgFigure' + index}/>);
     });
     return (
-      <section className="stage">
+      <section className="stage" ref="stage">
         <section className="img-sec">
           {imgFigures}
         </section>
